0c8fde907ee71d5690ce41ccc7a092f35a409ee9
Jenny Zhu <jz2915@nyu.edu>
2020-10-23 22:16:58 +0800

    Save changes to sql/ddl

--- app/models.py
+++ app/models.py
@@ -1,5 +1,17 @@
 from app import db
 
+class Hospital(db.Model):
+	pass
+
+class Department(db.Model):
+    pass
+
+class Doctor(db.Model):
+    pass
+
+class Nurse(db.Model):
+    pass
+
 class Patient(db.Model):
     pass
 
