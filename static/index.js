d7ea65dfc31f1d2fba51037575dfd1b5e3b70653
SZTankWang <2938599137@qq.com>
2020-10-23 23:00:16 +0800

    病人导航页

---
+++ static/index.js
@@ -0,0 +1,36 @@
+$(document).ready(renderNotice());
+
+//添加近期预约
+function renderNotice(data){
+	//测试数据
+	var arr = new Array();
+	var date = new Date();
+
+	//console.log(date);
+	arr.push({'id':1,'time':date,'hospital':'医院','dept':'科室','doctor':'医生','state':'未开始'});
+	arr.push({'id':2,'time':date,'hospital':'医院','dept':'科室','doctor':'医生','state':'未开始'});
+	arr.push({'id':3,'time':date,'hospital':'医院','dept':'科室','doctor':'医生','state':'已结束'});
+
+	//拼接notice
+	for(var i=0;i<arr.length;i++){
+		var temp ='';
+		temp +='<div class="recent"><div class="time">'+arr[i].time+'</div>';
+		console.log(arr[i].time);
+		temp +='<div class="info"><div class="info-content">'+arr[i].hospital+'</div>';
+		temp += '<div class="info-content">'+arr[i].dept+'</div><div class="info-content">'+arr[i].doctor+'</div><div class="info-content" id='+arr[i].id+'>'+arr[i].state+'</div>';
+		$("#outer-container").append(temp);
+
+		var id=arr[i].id;
+		var state=arr[i].state;
+		renderState(state,id);
+	}
+}
+//根据预约状态渲染状态 div 颜色
+function renderState(state,id){
+	if(state=='未开始'){
+		document.getElementById(id).style.background='#53AD06';
+	}
+	else{
+		document.getElementById(id).style.background='#AEB6BF';
+	}
+}
