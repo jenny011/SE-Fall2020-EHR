e156d3576ada52774dfeb8abbf4c8c936835c6f2
Jenny Zhu <jz2915@nyu.edu>
2020-10-23 15:16:07 +0800

    Add ddls; add initial inserts

--- sql/ehr_ddl.sql
+++ sql/ehr_ddl.sql
@@ -11,29 +11,59 @@ create table hospital (
 --Department
 create table department (
 	id varchar(20) NOT NULL,
-	hospital_id varchar(20),
+	hospital_id varchar(20) REFERENCES hospital(id) 
+	ON UPDATE CASCADE ON DELETE CASCADE,
 	name varchar(100) NOT NULL,
 	phone varchar(20),
 	address text,
 	description longtext,
-	foreign key (hospital_id) references hospital(id)
+	primary key (id)	
 );
 
 --Doctor
 create table doctor (
 	id varchar(20),
-	department_id varchar(20),
+	department_id varchar(20) REFERENCES department(id)
+	ON UPDATE CASCADE ON DELETE SET NULL,
+	hospital_id varchar(20) REFERENCES hospital(id) 
+	ON UPDATE CASCADE ON DELETE SET NULL,
 	password text NOT NULL,
 	name varchar(100) NOT NULL,
+	email varchar(100) UNIQUE NOT NULL,
 	phone varchar(20),
 	address text,
-	primary key (id),
-	foreign key (department_id) references department(id)
+	primary key (id)
 );
 
 --Nurse
+create table nurse (
+	id varchar(20),
+	department_id varchar(20) REFERENCES department(id)
+	ON UPDATE CASCADE ON DELETE SET NULL,
+	hospital_id varchar(20) REFERENCES hospital(id) 
+	ON UPDATE CASCADE ON DELETE SET NULL,
+	password text NOT NULL,
+	name varchar(100) NOT NULL,
+	email varchar(100) UNIQUE NOT NULL,
+	phone varchar(20),
+	address text,
+	primary key (id)
+);
 
 --Patient
+create table patient (
+	id varchar(20),
+	password text NOT NULL,
+	name varchar(100) NOT NULL,
+	email varchar(100) UNIQUE NOT NULL,
+	phone varchar(20),
+	address text,
+	age tinyint UNSIGNED,
+	gender enum('male', 'female'),
+	blood type enum('A', 'B', 'O', 'AB', 'RH+', 'RH-', 'other'),
+	allergies text,
+	primary key (id)
+);
 
 --Time slot
 
@@ -44,5 +74,24 @@ create table doctor (
 --Prescription
 
 --Lab repot
+create table lab_report (
+	id varchar(20),
+	type text REFERENCES lab_report_type(id)
+	ON UPDATE CASCADE,
+	medical_record_id varchar(20) REFERENCES medical_record(id)
+	ON UPDATE CASCADE,
+	uploader_id varchar(20) REFERENCES nurse(id)
+	ON UPDATE CASCADE,
+	patient_id varchar(20) REFERENCES patient(id)
+	ON UPDATE CASCADE,
+	file longblob,
+	comments longtext,
+	primary key (id)
+);
 
 --Lab report type
+create table lab_report_type (
+	type text,
+	description longtext,
+	primary key (type)
+);
