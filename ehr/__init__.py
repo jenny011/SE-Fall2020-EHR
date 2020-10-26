8c32f487a8f50b444943885fa42b1b9e96eba041
Qing Deng <qd319@nyu.edu>
2020-10-26 14:50:00 +0800

    updates

--- ehr/__init__.py
+++ ehr/__init__.py
@@ -3,12 +3,19 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 # dialect + connector
-app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://root:password@localhost/wecare"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db = SQLAlchemy(app)
+try:
+	# app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://root:password@127.0.0.1/wecare"
+	app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://root:password@localhost/wecare"
+	db = SQLAlchemy(app)
+except:
+	app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://root:password@127.0.0.1/wecare"
+	# app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://root:password@localhost/wecare"
+	db = SQLAlchemy(app)
 
 if __name__ == '__main__':
 	print("starting ehr system...")
+	
 	app.run(debug=True)
 
 # import building blocks
