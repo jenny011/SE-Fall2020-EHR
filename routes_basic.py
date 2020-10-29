e39735d28da76f69628f28797c3e08b57a6a6696
Jenny Zhu <jz2915@nyu.edu>
2020-10-29 23:53:45 +0800

    Front+Back working register: update frontend form and request type; update models; update register

--- routes_basic.py
+++ routes_basic.py
@@ -1,11 +1,13 @@
 from operator import ne
 from flask import Flask, render_template, redirect, url_for, request, json, jsonify, session
-# from flask_login.utils import logout_user
+from flask_login.utils import logout_user
 from flask_login import login_user, logout_user, current_user
 from uuid import uuid4
+from werkzeug.security import check_password_hash, generate_password_hash
 # from ehr.forms import LoginForm, RegisterForm
 from SE_Fall2020_EHR import app, db, login
 from SE_Fall2020_EHR.models import *
+import sys
 
 @login.user_loader
 def load_user(user_id): # haven't decided which identifier to use. ID or Email?
@@ -30,41 +32,41 @@ def register():
 	if current_user.is_authenticated:
 		return redirect(url_for(f'{user.role}+Home'))
 	role = request.form['role']
-	id = request.form['id']
 	first_name = request.form['firstName']
 	last_name = request.form['lastName']
-	email = request.form['email']
+	id = request.form['id']
 	phone = request.form['phone']
+	email = request.form['email']
 	password = request.form['password']
-	try:
+	# try:
 		# get pre-registered doctor/nurse
-		if role == "doctor":
-			doctor = Doctor.query.get(id)
-			if not doctor:
-				return {"error": "No pre-registered by admin"}
-		elif role == "nurse":
-			nurse = Nurse.query.get(id)
-			if not nurse:
-				return {"error": "No pre-registered by admin"}
-		# generate random unique user_id and create new user
-		user_id = uuid4()
-		user = User(user_id, first_name, last_name, role, email, phone)
-		user.set_password(password)
-		# update corresponding table
-		if role == "patient":
-			patient = Patient(id, user_id)
-			db.session.add(patient)
-		elif role == "doctor":
-			doctor.user_id = user_id
-		elif role == "nurse":
-			nurse.user_id = user_id
-		db.session.add(user)
+	if role == "doctor":
+		doctor = Doctor.query.get(id)
+		if not doctor:
+			return ""
+	elif role == "nurse":
+		nurse = Nurse.query.get(id)
+		if not nurse:
+			return ""
+	# generate random unique user_id and create new user
+	user_id = str(uuid4())
+	user = User(user_id=user_id, first_name=first_name, last_name=last_name, role=role, email=email, phone=phone, password_hash=generate_password_hash(password))
+	db.session.add(user)
+	db.session.commit()
+	# update corresponding table
+	if role == "patient":
+		patient = Patient(national_id=id, user_id=user_id)
+		db.session.add(patient)
 		db.session.commit()
-		return redirect(url_for('login'))
-	except:
-		db.session.rollback()
-		print(sys.exc_info()[0])
-		return sys.exc_info()[0]
+	elif role == "doctor":
+		doctor.user_id = user_id
+	elif role == "nurse":
+		nurse.user_id = user_id
+	return redirect(url_for('login'))
+	# except:
+	# 	db.session.rollback()
+	# 	print('error')
+	# 	return "error"
 
 
 	# another possible style of coding
@@ -100,14 +102,14 @@ def login():
 		if not current_user.is_authenticated:
 			email = request.form['email']
 			password = request.form['password']
-			try:
-				user = User.query.one(id)
-				match = user.check_password(password)
-				if not match:
-					return redirect(url_for('login'))
-				login_user(user)
-			except:
+			# try:
+			user = User.query.one(id)
+			match = user.check_password(password)
+			if not match:
 				return redirect(url_for('login'))
+			login_user(user)
+			# except:
+				# return redirect(url_for('login'))
 		return redirect(url_for(f'{user.role}+Home'))
 
 	# return render_template('/login.html', form=form)
