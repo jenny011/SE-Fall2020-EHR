2113b7827b71f2ac9109ababfbb56c455cd6c0ee
Qing Deng <qd319@nyu.edu>
2020-10-30 15:33:11 +0800

    precent re-register

--- routes_basic.py
+++ routes_basic.py
@@ -19,17 +19,17 @@ def load_user(user_id): # haven't decided which identifier to use. ID or Email?
 def home():
 	return render_template('WeCare.html')
 
-# @app.route('/goToLogin')
-# def goToLogin():
-# 	return render_template('login.html')
-
 #-------------------Register--------------------
 #-------------------Register--------------------
 
 @app.route('/register', methods=['POST'])
 def register():
+	"""
+		patient: register by (national)id
+		doctor/nurse: register by (license)id
+	"""
 	if current_user.is_authenticated:
-		return redirect(url_for(f'{user.role}+Home'))
+		return redirect(url_for(f'{current_user.role}+Home'))
 	role = request.form['role']
 	first_name = request.form['firstName']
 	last_name = request.form['lastName']
@@ -37,93 +37,74 @@ def register():
 	phone = request.form['phone']
 	email = request.form['email']
 	password = request.form['password']
-	# try:
+	try:
 		# get pre-registered doctor/nurse
-	if role == "doctor":
-		doctor = Doctor.query.get(id)
-		if not doctor:
-			return ""
-	elif role == "nurse":
-		nurse = Nurse.query.get(id)
-		if not nurse:
-			return ""
-	# generate random unique user_id and create new user
-	user_id = str(uuid4())
-	user = User(user_id=user_id, first_name=first_name, last_name=last_name, role=role, email=email, phone=phone, password_hash=generate_password_hash(password))
-	db.session.add(user)
-	db.session.commit()
-	# update corresponding table
-	if role == "patient":
-		patient = Patient(national_id=id, user_id=user_id)
-		db.session.add(patient)
+		if role == "doctor":
+			doctor = Doctor.query.get(id)
+			if not doctor:
+				return {"error": "No pre-registered by admin"}
+			# if already owned a user_id, prevent them from obtaining a new one
+			elif doctor and doctor.user_id:
+				return {"error": 'Already registered!'}
+		elif role == "nurse":
+			nurse = Nurse.query.get(id)
+			if not nurse:
+				return {"error": "No pre-registered by admin"}
+			# if already owned a user_id, prevent them from obtaining a new one
+			elif nurse and nurse.user_id:
+				return {"error": 'Already registered!'}
+		elif role == "patient":
+			patient = Patient.query.get(id)
+			# if already exist, prevent from register again
+			if patient:
+				return {"error": 'Already registered!'}
+
+		# generate random unique user_id and create new user
+		user_id = uuid4()
+		user = User(user_id, first_name, last_name, role, email, phone)
+		user.set_password(password)
+
+		# update corresponding table
+		if role == "patient":
+			patient = Patient(id, user_id)
+			db.session.add(patient)
+		elif role == "doctor" or role == 'nurse':
+			# update pre-registered info
+			eval(role).user_id = user_id
+
+		db.session.add(user)
 		db.session.commit()
-	elif role == "doctor":
-		doctor.user_id = user_id
-	elif role == "nurse":
-		nurse.user_id = user_id
-	return redirect(url_for('login'))
-	# except:
-	# 	db.session.rollback()
-	# 	print('error')
-	# 	return "error"
-
-
-	# another possible style of coding
-	# form = RegisterForm()
-	# if form.validate_on_submit():
-	# 	flash(f'Register for a {form.role.data} with id: {form.id.data}')
-	# 	user = User(
-	# 				id=form.id.data,
-	# 				email=form.email.data,
-	# 				role=form.role.data,
-	# 				first_name = form.first_name.data,
-	# 				last_name = form.last_name.data,
-	# 				phone = form.phone.data
-	# 			)
-	# 	user.set_password(form.password.data)
-	# 	db.session.add(user)
-	# 	db.session.commit()
-	# 	flash("Welcome to the family!")
-	# 	return redirect(url_for('login'))
-	# return render_template('register.html', form=form)
-
+		return redirect(url_for('login'))
+	except:
+		db.session.rollback()
+		print(sys.exc_info()[0])
+		return sys.exc_info()[0]
 
 #--------------------Login---------------------
 #--------------------Login---------------------
-#login_required
-#authentication
 
 @app.route('/login', methods=['GET','POST'])
 def login():
+	"""
+		patient login with: national id + password
+		doctor/patient login with: license id + password
+	"""
 	if request.method == 'GET':
 		return render_template('login.html')
 	if request.method == 'POST':
 		if not current_user.is_authenticated:
-			email = request.form['email']
 			password = request.form['password']
-			# try:
-			user = User.query.one(id)
-			match = user.check_password(password)
-			if not match:
+			try:
+				user = User.query.one(id)
+				if not user or not user.check_password(password):
+					flash("Unregistered ID or wrong password")
+					return redirect(url_for('login'))
+				login_user(user)
+			except:
+				flash("Unknown error, sorry!")
 				return redirect(url_for('login'))
-			login_user(user)
-			# except:
-				# return redirect(url_for('login'))
-		return redirect(url_for(f'{user.role}+Home'))
+		return redirect(url_for(f'{current_user.role}+Home'))
 
-	# return render_template('/login.html', form=form)
-
-	# form = LoginForm()
-	# if form.validate_on_submit():
-	# 	flash(f'Login for a {form.role.data} with id: {form.id.data}')
-	# 	user = User.query.filter_by(id=form.id.data).first()
-	# 	if not user or not user.check_password(form.password.data):
-	# 		flash("Invalid ID or unmatched Password")
-	# 		return redirect(url_for('login'))
-	#
-	# 	# session['role_type'] = form.role.data
-	# 	login_user(user) # current_user becomes user
-	# 	return redirect(url_for(f'{user.role}+Home'))
 
 
 #--------------------Logout---------------------
@@ -131,5 +112,8 @@ def login():
 
 @app.route('/logout', methods=['POST'])
 def logout():
+	"""
+		logout the system
+	"""
 	logout_user()
-	return redirect(url_for('/'))
+	return redirect(url_for('home'))
