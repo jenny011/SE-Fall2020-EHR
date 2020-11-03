63f5374ad83c32226d768b361a24edbabfc7353f
SZTankWang <2938599137@qq.com>
2020-11-03 13:30:45 +0800

    上传了index.py 供前端调试使用

---
+++ index.py
@@ -0,0 +1,50 @@
+from flask import Flask
+from flask import render_template,request
+app = Flask(__name__)
+
+@app.route('/')
+def helloWorld():
+	return render_template('index.html')
+
+@app.route('/WeCare/homepage')
+def WeCare():
+	return render_template('WeCare.html')
+
+@app.route('/WeCare')
+def loginForm():
+	return render_template('login.html')
+
+# @app.route('/WeCare/register')
+# def registerForm():
+# 	return render_template('register.html')
+
+@app.route('/WeCare/choice')
+def choice():
+	return render_template('choice.html')
+
+@app.route('/WeCare/register')
+def handleRegister():
+	registerType = request.args.get('register-role')
+	print(registerType)
+	return "success"
+
+@app.route('/WeCare/login')
+def handleLogin():
+	# registerType = request.args.get('register-role')
+	# print(registerType)
+	return "success"
+
+@app.route('/register')
+def register():
+	return render_template('register.html')
+
+@app.route('/loginTemplate')
+def loginTemplate():
+	return render_template('temp_login.html')
+
+@app.route('/hospital')
+def hospitalTemplate():
+	return render_template('hospitalListPage.html')
+
+if __name__ == '__main__':
+	app.run(debug=True)    
