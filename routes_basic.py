a26de3c3b98c8d37daafea1258a48da0fb0db021
Jenny Zhu <jz2915@nyu.edu>
2020-11-06 20:57:01 +0800

    修改login步骤

--- routes_basic.py
+++ routes_basic.py
@@ -68,8 +68,8 @@ def register():
 #--------------------Login---------------------
 #--------------------Login---------------------
 
-@app.route('/login', methods=['GET','POST'])
-def login():
+@app.route('/loginRequest', methods=['GET','POST'])
+def loginRequest():
 	"""
 		patient login with: national id + password
 		doctor/patient login with: license id + password
@@ -93,7 +93,6 @@ def login():
 			except:
 				# flash("Unknown error, sorry!")
 				return make_response(jsonify({"ret": "Unknown error"}))
-
 		return make_response(jsonify({"ret":0, "role":current_user.role.value, "id": current_user.id.value}), 200)
 		#redirect(url_for(f'{current_user.role.value}Home'))
 
@@ -108,10 +107,10 @@ def logout():
 
 #--------------------home---------------------
 #--------------------home---------------------
-@app.route('/patientHome', methods=['GET'])
+@app.route('/login', methods=['GET'])
 @login_required
 def patientHome():
-	return render_template('PatientHome.html')
+	return render_template(f'{current_user.role.value}Home.html')
 
 
 #--------------------get hospital list data---------------------
